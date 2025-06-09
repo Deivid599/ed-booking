0e98a98b52627a9c596ff721858c1502526f744b
Deivid599 <92714825+Deivid599@users.noreply.github.com>
2025-06-09 18:40:50 +0200

    Creada la clase RESERVA, y sus constructores

---
+++ src/RESERVA.java
@@ -0,0 +1,47 @@
+public class RESERVA {
+    private String dniUsuario;
+    private int idHabitacion;
+    private String fechaInicio;
+    private String fechaFin;
+    private double precio;
+
+    public String getDniUsuario() {
+        return dniUsuario;
+    }
+
+    public void setDniUsuario(String dniUsuario) {
+        this.dniUsuario = dniUsuario;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
