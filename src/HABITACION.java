bf24d1d29d5f6893ad3d9ddd0b56de9b86c36310
Deivid599 <92714825+Deivid599@users.noreply.github.com>
2025-06-09 18:35:20 +0200

    Creada la clase HABITACION, y sus constructores

---
+++ src/HABITACION.java
@@ -0,0 +1,29 @@
+public class HABITACION {
+    private int id;
+    private int numero;
+    private  String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
